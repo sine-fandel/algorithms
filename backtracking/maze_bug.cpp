4056527dabaca186adbf340398f23e45b69c6158
fang <uncleduckfzx@gmail.com>
2017-12-03 10:02:21 +0800

    Update maze_bug.cpp
    增加了判断位置是否访问过的语句，解决了bug

--- backtracking/maze_bug.cpp
+++ backtracking/maze_bug.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 int top = -1;   //-1表示还没开始走
 
+//若迷宫该位置访问过则为1
+int maze_vis[5][5] = {
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0}
+};
+
 //迷宫数组，0:可行 1:不可行
 int maze[5][5]={
     {0, 0, 0, 0, 0},
@@ -26,15 +35,16 @@ struct place
 }p[100];    
 
 //回溯法求路径 (xi, yi)表示当前位置 (xe, ye)表示目标位置
-void backtracking (int xi, int yi, int xe, int ye)
+void backtracking (int xi, int yi, int xe, int ye)    //通过返回值好确定有没有找到一条通路
 {
     if (xe == xi && ye == yi)     //若到达目的地则输出路径
     {
         cout << "迷宫路径如下：" << endl;
         for (int i = 0; i <= top; i++)
           cout << "(" << p[i].col << ", " << p[i].row << ") -> ";
+        cout << "(" << xe << ", " << ye << ")";
         cout << endl;
-        return;
+        //return true;
     }
     else
     {
@@ -42,6 +52,7 @@ void backtracking (int xi, int yi, int xe, int ye)
         top += 1;
         p[top].col = xi;
         p[top].row = yi;
+        maze_vis[xi][yi] = 1;
         for (int i = 0; i < 4; i++)   //搜索这位置的可行路径
         {
             switch (i)
@@ -51,19 +62,22 @@ void backtracking (int xi, int yi, int xe, int ye)
                 case 2 :  x = xi - 1;   y = yi;   break;
                 case 3 :  x = xi;   y = yi - 1;   break;
             }
-            if (x >= 0 && y >= 0 && maze [y][x] == 0)   //若为通路
+            //if(p[top - 1].col == x && p[top - 1].row == y) continue;    //在新的一点找路的时候，别找回头路
+            if ((x >= 0 && x < 5) && (y >= 0 && y < 5) && maze [x][y] == 0 && maze_vis[x][y] == 0)   //若为通路，x和y到达边界的时候，不要再判断了，不然maze都越界了
             {
                 backtracking (x, y, xe, ye);
+                //if(bRet) return true;    //找到了就不用再执行了
+                maze_vis[x][y] = 0;
                 top -= 1;
             }
         }
     }
+    //return false;
 }
-
 int main ()
 {
     cout << "请输入目的地：";
     int xe, ye;
     cin >> xe >> ye;    
-    backtracking (0, 0, 4, 4);
+    backtracking (0, 0, xe, ye);
 }
